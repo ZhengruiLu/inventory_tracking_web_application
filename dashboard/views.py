da07263c926349199c2a3ae2be11cdb558b33aed
ZhengruiLu <lu.zhengr@northeastern.edu>
2022-05-20 21:17:41 -0700

    update for UI

--- dashboard/views.py
+++ dashboard/views.py
@@ -4,5 +4,13 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse('This is the Index Page')
+    return render(request, 'dashboard/index.html')
 
+def staff(request):
+    return render(request, 'dashboard/staff.html')
+
+def product(request):
+    return render(request, 'dashboard/product.html')
+
+def order(request):
+    return render(request, 'dashboard/order.html')
--- inventory_tracking_web_application/settings.py
+++ inventory_tracking_web_application/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'inventory_tracking_web_application.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,7 +118,17 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS =[
+    BASE_DIR/ "static"
+]
+
+STATIC_ROOT = (BASE_DIR/"/asert/")
+
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
