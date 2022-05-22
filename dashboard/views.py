5059ea21ff80e629ab36339fd5589f0362d8edb7
ZhengruiLu <lu.zhengr@northeastern.edu>
2022-05-21 19:59:07 -0700

    update for new app user

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,9 +1,17 @@
+from multiprocessing import context
 from django.shortcuts import render
 from django.http import HttpResponse
-
+# from .models import Product
 # Create your views here.
 
 def index(request):
+    # items = Product.objects.all()
+    # items = Product.objects.raw('SELECT * FROM dashboard_product')
+
+    # context = {
+    #     'items': items,
+    # }
+
     return render(request, 'dashboard/index.html')
 
 def staff(request):
