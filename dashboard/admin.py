0d15c9dde7ae4dd10741615873c07d40c25994ec
ZhengruiLu <lu.zhengr@northeastern.edu>
2022-05-21 17:03:19 -0700

    update for more models

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,6 +1,14 @@
 import imp
 from django.contrib import admin
-from .models import Product
+from .models import Order, Product
+from django.contrib.auth.models import Group
+
+admin.site.site_header = 'SuperInventory Dashboard'
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'quantity',)
+    list_filter = ['category']
 
 # Register your models here.
-admin.site.register(Product)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Order)
