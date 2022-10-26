96f6fc56ee6f15fe735886282cce9bff6d34c4f5
ZhengruiLu <lu.zhengr@northeastern.edu>
2022-10-25 22:06:48 -0700

    update for adding comments

--- inventory_tracking_web_application/settings.py
+++ inventory_tracking_web_application/settings.py
@@ -29,14 +29,14 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
-
 INSTALLED_APPS = [
+    # init
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    'django.contrib.staticfiles',#init end
     'dashboard.apps.DashboardConfig',#register the app dashboard
     'user.apps.UserConfig',
     'crispy_forms',
